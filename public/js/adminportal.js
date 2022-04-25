b00de53f424775815194b4771aa882300bdafd6a
sarat <ksaratc@gmail.com>
2022-04-26 01:07:56 +0530

    commit1-2

--- public/js/adminportal.js
+++ public/js/adminportal.js
@@ -2,13 +2,28 @@ let searchanevent =document.getElementById('searchanevent');
 let deleteevent = document.getElementById('deleteevent');
 let searchauser = document.getElementById('searchauser');
 let deleteuser = document.getElementById('deleteuser');
-let temporaryban = document.getElementById('temporaryban');
-let warning = document.getElementById('warning');
 let eventstats = document.getElementById('eventstats');
 let cards = document.getElementsByClassName('card');
 cards = Array.from(cards);
 
+const displayusers = async () => {
+    try {
+      
+      const string = 'http://localhost:3001/api/user/allUsers';
+      const res = await fetch(
+          string, {headers: {
+            "Content-Type": "application/json",
+          },}
+          );
+      const information = await res.json();
+      console.log(information);
+          return information;
+      } catch (err) {
+          console.log(err);
+      }
+  };
 
+displayusers();
 
 searchanevent.addEventListener('click', ()=>{
     let a = document.getElementById('card1');
@@ -37,30 +52,6 @@ searchauser.addEventListener('click', ()=>{
 
 deleteuser.addEventListener('click', ()=>{
     let a = document.getElementById('card4');
-    cards.forEach(element => {
-        element.style.display = 'none'; 
-    });
-    a.style.display = 'flex';
-})
-
-temporaryban.addEventListener('click', ()=>{
-    let a = document.getElementById('card5');
-    cards.forEach(element => {
-        element.style.display = 'none'; 
-    });
-    a.style.display = 'flex';
-})
-
-warning.addEventListener('click', ()=>{
-    let a = document.getElementById('card6');
-    cards.forEach(element => {
-        element.style.display = 'none'; 
-    });
-    a.style.display = 'flex';
-})
-
-eventstats.addEventListener('click', ()=>{
-    let a = document.getElementById('card7');
     cards.forEach(element => {
         element.style.display = 'none'; 
     });
